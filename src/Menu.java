cd82f3fb0014a4753ff74021f9731cf08ffd73dd
Ruben-Rey <ibiza.reymundis@gmail.com>
2023-01-18 15:21:44 -0500

    Inplements validators

--- src/Menu.java
+++ src/Menu.java
@@ -20,25 +20,37 @@ public class Menu {
     Scanner scanner = new Scanner(System.in).useDelimiter("\n");
  
     String exit = "yes";
-    int option = 0;
-
+    String input;
+    
     while(exit.equals("yes")){
-        System.out.println("---------- Music library ---------- \n"
-                        + "---------------------------------------- \n"
-                        + "1 - Create playlist \n" 
-                        + "2 - Add song to playlist \n"
-                        + "3 - Filter music by artist \n"
-                        + "4 - Filter music by genre \n"
-                        + "5 - Filter music by year \n"
-                        + "6 - Sort by duration \n"
-                        + "7 - Sort by year \n"
-                        + "8 - Show playlist \n"
-                        );
-                        
-        option = scanner.nextInt();
-        String saltar = scanner.nextLine();
+        do{
+            System.out.println("---------- Music library ---------- \n"
+                            + "---------------------------------------- \n"
+                            + "1 - Create playlist \n" 
+                            + "2 - Add song to playlist \n"
+                            + "3 - Filter music by artist \n"
+                            + "4 - Filter music by genre \n"
+                            + "5 - Filter music by year \n"
+                            + "6 - Order by length ascending \n"
+                            + "7 - Order by length descending \n"
+                            + "8 - Order by year ascending \n"
+                            + "9 - Order by year descending \n"
+                            + "10 - Show playlist \n"
+                            );
+                            
+            input = scanner.nextLine();
+
+        }while(!Validators.isInteger(input));
+
+        while(!Validators.optionRange(input)){
+            System.out.println("Ingrese opciones del 1 - 10");
+            input = scanner.nextLine();
+        }
+
+        int option = Integer.parseInt(input);
+
         Song song = new Song();
-        
+                
         switch(option){
             case 1:
                 System.out.println("Create playlist");
@@ -70,7 +82,7 @@ public class Menu {
                 break;
 
                 
-                case 2:
+            case 2:
                 System.out.println("---------- Enter song data ---------- \n"
                 + "Artist" ); 
                 String artist = scanner.nextLine();
@@ -86,7 +98,7 @@ public class Menu {
                 
                 System.out.println("Duration");
                 int length = scanner.nextInt();
-                saltar = scanner.nextLine();
+                String skipLine = scanner.nextLine();
                 
                 System.out.println("Genre");
                 String genre = scanner.nextLine();
@@ -115,21 +127,41 @@ public class Menu {
 
             case 5:
                 System.out.println("Filter music by year");
+                song.filterByYear(playlist);
                 break;
             
             case 6:
-                System.out.println("Sort by duration");
+                System.out.println("Order by length ascending");
+                Collections.sort(playlist, Comparators.orderByLengthAsc);
+                showPlaylist(playlist);
+                Collections.sort(playlist, Comparators.orderById);
+                
                 break;
-
+                
             case 7:
-                System.out.println("Sort by year"); 
+                System.out.println("Order by length descending");
+                Collections.sort(playlist, Comparators.orderByLengthDesc);
+                showPlaylist(playlist);
+                Collections.sort(playlist, Comparators.orderById);
                 break;
 
             case 8:
-                System.out.println("show playlist");
+                System.out.println("Order by year ascending");
+                Collections.sort(playlist, Comparators.orderByDateAsc);
                 showPlaylist(playlist);
-
+                Collections.sort(playlist, Comparators.orderById);
                 break;
+
+            case 9:
+                System.out.println("Order by year descending");
+                Collections.sort(playlist, Comparators.orderByDateDesc);
+                showPlaylist(playlist);
+                Collections.sort(playlist, Comparators.orderById);
+                break;
+            
+            case 10:
+                System.out.println("Show playlist");
+                showPlaylist(playlist);
         }
 
         System.out.println("Do you want to continue in the application? [Yes/Not]");
--- src/Validators.java
+++ src/Validators.java
@@ -11,9 +11,26 @@ public class Validators {
      * Valida las entradas si son numeros o cadenas de caracteres
      */
 
-     public static boolean isNumeric(int option){
-        return false;
+   public static Boolean isInteger(String input){
+      int number;
 
+      try {
+          number = Integer.parseInt(input);
+          return true;
+      } catch (NumberFormatException e) {
+          // TODO: handle exception
+          return false;
+      }
+   }
+   
 
-     }
+   public static Boolean optionRange(String input){
+        
+      int numero = Integer.parseInt(input); 
+ 
+      if(numero >= 1 && numero <= 10){
+         return true;
+      }
+         return false;
+     } 
 }
